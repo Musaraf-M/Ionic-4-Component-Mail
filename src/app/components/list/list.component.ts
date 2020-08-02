3c163e39c6aaae80297cce0e849aa4dcdd6c90bb
Musaraf2000 <mohammedmusaraf.psnacet.edu.in>
2020-08-02 17:01:13 +0530

    [IMP] improvements had to be done has ERR

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  @Input() list: Object;
+  @Output() delete = new EventEmitter<Object>();
 
-  constructor() { }
+  constructor() {}
 
+  // emit the event to the parent
+  deleteItem() {
+    this.delete.emit(this.list);
+  }
   ngOnInit() {}
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,67 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+    entryList: Object[] = [
+      { "id": 1, "name": 'Musaraf' , "subject": 'Hello hi'},
+      { "id": 2, "name": 'Musaraf' , "subject": 'Hello hi'},
+      { "id": 3, "name": 'Musaraf' , "subject": 'Hello hi'}
+    ];
+  
 
+
+  constructor(public alertController: AlertController) {}
+
+  deleteItem(entryList: Object) {
+    // filter the given item and store it in the same array
+    this.entryList = this.entryList.filter((element) => {
+      return element["id"] !== entryList["id"];
+    });
+  }
+
+
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Add Mail',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Enter name'
+        },
+        {
+          name: 'subject',
+          type: 'text',
+          placeholder: 'Enter subject'
+        }
+        
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log('Confirm Ok');
+            
+            console.log(data.name,data.subject);
+            const length = this.entryList.length - 1;
+            const maxId = this.entryList[length]["id"];
+
+    // add an item to the array
+    this.entryList.push({ id: maxId + 1, name: data.name, subject: data.subject });
+
+            
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
